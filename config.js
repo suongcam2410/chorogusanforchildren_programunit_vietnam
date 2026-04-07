02fdfd149077cf454eb34a82807b1d6fa830fe7e
Suong Cam <suongcam2410@gmail.com>
2026-04-07 17:58:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   // Replace this value after you deploy the Apps Script backend as a Web App.
-  API_BASE_URL: 'https://script.google.com/macros/s/AKfycbxPId8C7jc5n_wyA5pWMOPGy6efQRIHKKStMHeJnD2tJ9asIXiQvFjHeqqsIVHsGVwR4w/exec',
+  API_BASE_URL: 'https://script.google.com/macros/s/AKfycbxPId8C7jc5n_wyA5pWMOPGy6efQRIHKKStMHeJnD2tJ9asIXiQvFjHeqqsIVHsGVwR4w/',
 
   // Google Sheet requested by the user
   SPREADSHEET_URL: 'https://docs.google.com/spreadsheets/d/1qOeUB-NCxspsVLvxUXzZuVIt8ffetuezBIydqKiBOxc/',
