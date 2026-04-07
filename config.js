6c42dc24ea7c8595e8ac21c7ca526ba95ce3c0c8
Suong Cam <suongcam2410@gmail.com>
2026-04-07 17:31:47 +0700

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-window.APP_CONFIG = {
-  // Replace this value after you deploy the Apps Script backend as a Web App.
-  API_BASE_URL: 'https://script.google.com/macros/s/AKfycbwxUyO5xl_N_4E8BiVEZViUSVSs3wCseDeGXW0LdpM6oOWWCryq6YR7ZcM5cocMyD1c5Q/exec',
-
-  // Google Sheet requested by the user
-  SPREADSHEET_URL: 'https://docs.google.com/spreadsheets/d/1qOeUB-NCxspsVLvxUXzZuVIt8ffetuezBIydqKiBOxc/',
-
-  APP_TITLE: 'ChorogUsan Working Plan Dashboard'
-};
