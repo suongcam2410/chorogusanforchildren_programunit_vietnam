1ba4ac70cd6d6051004465bb37e4dfa1aec926c8
Suong Cam <suongcam2410@gmail.com>
2026-04-07 16:55:27 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+window.APP_CONFIG = {
+  // Replace this value after you deploy the Apps Script backend as a Web App.
+  API_BASE_URL: 'PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE',
+
+  // Google Sheet requested by the user
+  SPREADSHEET_URL: 'https://docs.google.com/spreadsheets/d/1qOeUB-NCxspsVLvxUXzZuVIt8ffetuezBIydqKiBOxc/',
+
+  APP_TITLE: 'ChorogUsan Working Plan Dashboard'
+};
