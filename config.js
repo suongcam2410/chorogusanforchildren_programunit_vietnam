59d6da15c48341ddd01a31f1a24161eb5117734f
Suong Cam <suongcam2410@gmail.com>
2026-04-09 09:16:43 +0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-window.APP_CONFIG = {
-  // Paste your deployed Google Apps Script Web App /exec URL here to avoid configuring it inside the page.
-  // Example: 'https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx/exec'
-  API_BASE_URL: 'https://script.google.com/macros/s/AKfycbxPId8C7jc5n_wyA5pWMOPGy6efQRIHKKStMHeJnD2tJ9asIXiQvFjHeqqsIVHsGVwR4w/exec',
-
-  SPREADSHEET_URL: 'https://docs.google.com/spreadsheets/d/1qOeUB-NCxspsVLvxUXzZuVIt8ffetuezBIydqKiBOxc/',
-  APP_TITLE: 'ChorogUsan Working Plan Dashboard'
-};
